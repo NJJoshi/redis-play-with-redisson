d466075a4cca990bd09604952b2b017a79e8edd8
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-27 11:12:43 +0530

    Initial commit

---
+++ src/test/java/com/nj/play/redis/redisson/test/config/RedissonConfig.java
@@ -0,0 +1,25 @@
+package com.nj.play.redis.redisson.test.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.config.Config;
+
+import java.util.Objects;
+
+public class RedissonConfig {
+    private RedissonClient redissonClient;
+
+    public RedissonClient getRedissonClient() {
+        if(Objects.isNull(redissonClient)) {
+            Config config = new Config();
+            config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+            redissonClient = Redisson.create(config);
+        }
+        return redissonClient;
+    }
+
+    public RedissonReactiveClient getRedissonReactiveClient() {
+        return getRedissonClient().reactive();
+    }
+}
