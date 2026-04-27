873a1c42b2d73e3e8042547520e219127db47f48
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-27 13:25:54 +0530

    Added new test case about adding new Long object in redis

---
+++ src/test/java/com/nj/play/redis/redisson/test/NumberTest.java
@@ -0,0 +1,22 @@
+package com.nj.play.redis.redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RAtomicLongReactive;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class NumberTest extends BaseTest {
+
+    @Test
+    public void keyValueIncreaseTest() {
+        RAtomicLongReactive atomicLong = this.redissonClient.getAtomicLong("user:1:visit");
+        Mono<Void> mono = Flux.range(1,30)
+                                .delayElements(Duration.ofSeconds(1))
+                                .flatMap( i -> atomicLong.incrementAndGet())
+                                .then();
+        StepVerifier.create(mono).verifyComplete();
+    }
+}
