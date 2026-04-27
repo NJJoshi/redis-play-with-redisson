451e3d0088051c45c6c85b4eb96842a967b97c0a
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-27 13:52:02 +0530

    Added new test case about setting expired events on keys from redis and capture expiring event on redis via appropriate listener in code

---
+++ src/test/java/com/nj/play/redis/redisson/test/EventListenerTest.java
@@ -0,0 +1,28 @@
+package com.nj.play.redis.redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.ExpiredObjectListener;
+import org.redisson.api.RBucketReactive;
+import org.redisson.client.codec.StringCodec;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class EventListenerTest extends BaseTest {
+
+    @Test
+    public void expiredEventTest() {
+        RBucketReactive<String> bucket = this.redissonClient.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("NJ", Duration.ofSeconds(10));
+        Mono<Void> get = bucket.get().doOnNext(System.out::println).then();
+        Mono<Void> event = bucket.addListener(new ExpiredObjectListener() {
+            @Override
+            public void onExpired(String name) {
+                System.out.println("Expired event for : " + name);
+            }
+        }).then();
+        StepVerifier.create(set.concatWith(get).concatWith(event)).verifyComplete();
+        sleep(11000);
+    }
+}
