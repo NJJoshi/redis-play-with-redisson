ddc669e4dd89686e92990c7539bdb1e3ed49d7ae
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-29 16:52:40 +0530

    Added new test case about how do we perform complex addition using HyperLog log operation into redis

---
+++ src/test/java/com/nj/play/redis/redisson/test/HyperLogLogTest.java
@@ -0,0 +1,29 @@
+package com.nj.play.redis.redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RHyperLogLogReactive;
+import org.redisson.client.codec.LongCodec;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+public class HyperLogLogTest extends BaseTest{
+    @Test
+    public void testHyperLogLog(){
+        RHyperLogLogReactive<Long> counter = this.redissonClient.getHyperLogLog("usr:visits", LongCodec.INSTANCE);
+
+        List<Long> list1 =  LongStream.rangeClosed(1, 25000).boxed().collect(Collectors.toList());
+        List<Long> list2 =  LongStream.rangeClosed(25001, 50_000).boxed().collect(Collectors.toList());
+        List<Long> list3 =  LongStream.rangeClosed(50_001, 100_000).boxed().collect(Collectors.toList());
+        List<Long> list4 =  LongStream.rangeClosed(100_001, 150_000).boxed().collect(Collectors.toList());
+
+        Mono<Void> mono = Flux.just(list1,list2,list3,list4).flatMap(counter::addAll).then();
+        StepVerifier.create(mono).verifyComplete();
+
+        counter.count().doOnNext(System.out::println).subscribe();
+    }
+}
